6b520edb60a48e5ff7f686f9405058e29307780b
aayush <aayush6194@gmail.com>
2018-06-06 00:39:07 -0500

    task number

--- includes/tasks.php
+++ includes/tasks.php
@@ -2,19 +2,19 @@
 class  tasks extends db {
     public function getTasks () {
     if(isset($_SESSION['login'])){
-      if($_SESSION['login'])
-      {
-                    $sql = "SELECT * FROM todolist WHERE username='".$_SESSION['email']."' ORDER BY id  DESC LIMIT 5 ";
-                  }
+      if($_SESSION['login']){
+        $sql = "SELECT * FROM todolist WHERE username='".$_SESSION['email']."' ORDER BY id  DESC LIMIT 5 ";
+        }
       else{
           $sql = "SELECT * FROM todolist ORDER BY id  DESC LIMIT 5 ";
-      }
+          }
     }
     else {
         $sql = "SELECT * FROM todolist ORDER BY id  DESC LIMIT 5 ";
-    }
+              }
 
         $result = $this->connect()->query($sql);
+
         $numRows = $result->num_rows;
 
         if($numRows > 0){
@@ -26,6 +26,23 @@ class  tasks extends db {
         else {
           return false;
         }
+    }
+    public function getTasksNum () {
+      if($_SESSION['login']){
+          if($_SESSION['login']){
+            $sql2 = "SELECT * FROM todolist WHERE username='".$_SESSION['email']."' ORDER BY id  DESC ";
+          }
+          else{
+            $sql2 = "SELECT * FROM todolist ORDER BY id";
+          }
+    }
+    else {
+        $sql2 = "SELECT * FROM todolist ORDER BY id";
+    }
+
+        $numRsults = ($this->connect()->query($sql2))->num_rows;
+        return $numRsults;
+
     }
 }
 

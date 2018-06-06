b39eb420e106a06b89db9e63bd18c2cba232fb40
aayush <aayush6194@gmail.com>
2018-06-06 00:17:46 -0500

    bugs fixed

--- includes/db-inc.php
+++ includes/db-inc.php
@@ -6,19 +6,13 @@ class  db {
   private $dbname;
 
     public function connect () {
-      $this->servername = "localhost";
-      $this->username = "root";
-      $this->password = "";
-      $this->dbname = "todolists";
 
-      $conn = new mysqli ($this->servername, $this->username, $this->password, $this->dbname);
-      if(!$conn){
         $this->servername = "mysql2.gear.host";
          $this->username = "phptodolist1";
          $this->password = "poop123!";
          $this->dbname = "phptodolist1";
          $conn = new mysqli ($this->servername, $this->username, $this->password, $this->dbname);
-      }
+
       return $conn;
     }
 }
